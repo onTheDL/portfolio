9c09ba4bc631e928ebe591742f30329e378d2552
Daniel Laserna <daniel.laserna@protonmail.com>
2021-02-18 18:07:19 -0500

    Update hero text and project images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -27,8 +27,7 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
-            <br />
+            <span className="text-color-main">{name || 'Your Name'}</span> <br />
             {subtitle || "I'm the Unknown Developer."}
           </h1>
         </Fade>
--- src/mock/data.js
+++ src/mock/data.js
@@ -11,7 +11,7 @@ export const headData = {
 export const heroData = {
   title: 'Hello, my name is',
   name: 'Daniel',
-  subtitle: "I'm the Unknown Developer",
+  subtitle: "I'm the next Clutch React Developer",
   cta: 'Know more',
 };
 
@@ -22,7 +22,7 @@ export const aboutData = {
   paragraphTwo:
     "I love to build.  And when I stumbled on web development about a year ago, it was a natural fit. Having completed Codecademy's Web Developer Career Path, as well as Udacity’s React Nanodegree Program, I’ve trained myself to build responsive, scalable, and aesthetically professional web applications, with sights set to build enterprise-ready mobile applications as well.",
   paragraphThree:
-    'My current tech stack includes Javascript, ReactJS, NextJS, GatsbyJS, NodeJS, Firebase, Material UI, styled-components, HTML, CSS, Git, and VSCode',
+    'Current Stack:  Javascript, ReactJS, NextJS, GatsbyJS, NodeJS, Firebase, Material UI, styled-components, HTML, CSS, Git, and VSCode',
   resume: 'https://my.indeed.com/p/daniell-gb342sz',
 };
 
