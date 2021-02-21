0387c810684573016fd349075f6b7bdde18d81ca
Daniel Laserna <daniel.laserna@protonmail.com>
2021-02-21 15:03:29 -0500

    Update About info

--- src/mock/data.js
+++ src/mock/data.js
@@ -20,7 +20,7 @@ export const aboutData = {
   img: 'about.jpg',
   paragraphOne: "I'm a frontend developer, and I love what I do.",
   paragraphTwo:
-    "What started out as a hobby culminated into a full blown passion. I love the process of learning and finding ways to turn ideas into realized products. To that end, I've upgraded my web development skills by completing Codecademy's Web Developer Career Path, as well as Udacity’s React Nanodegree Program. I’ve trained myself to build responsive, scalable, and aesthetically professional software solutions with an eye for clean code. Consequently, I hope to use those skills to turn your great ideas into great products.",
+    "What started out as a hobby turned into a full blown passion. I love the process of learning and finding ways to develop ideas into realized products. To that end, I've completed Codecademy's Web Developer Career Path, as well as Udacity’s React Nanodegree Program. I’ve trained myself to build responsive, scalable, and aesthetically professional software solutions. Consequently, I hope to use those skills to turn your great ideas into great products.",
   paragraphThree:
     'Current Stack:  Javascript, ReactJS, NextJS, GatsbyJS, NodeJS, Firebase, Material UI, styled-components, HTML, CSS, Git, and VSCode',
   resume: 'https://my.indeed.com/p/daniell-gb342sz',
